1b5a4ea860746e140f05379b96e1befce4f1ab1f
zhifeng ou <zhifengou@hotmail.com>
2019-03-30 19:30:17 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -29,6 +29,7 @@ export default class ChromeBoisDomain extends Component {
     return (
       <canvas 
         onMouseMove={this.handleMouseMove}
+        
         width='900'
         height='600'
         tabIndex="0">
